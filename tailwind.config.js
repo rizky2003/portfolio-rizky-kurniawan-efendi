fecf04cd8f1763d81ab25d351eec899fcb567154
rizky <cadarislam99@gmail.com>
2022-04-13 14:00:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        dark: '#0f172a',
+        secondary:'#64748b', 
+      },
+    },
+  },
+  plugins: [],
+}
